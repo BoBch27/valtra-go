87bd1280e103a36b4e42ff2535c18d75821bf6e2
Bobby Donev <donev.bobby@gmail.com>
2025-11-23 16:58:03 +0000

    feat: add Min and Max validations for numeric types

--- valtra.go
+++ valtra.go
@@ -25,3 +25,29 @@ func Required[T comparable]() func(T) error {
 		return nil
 	}
 }
+
+type Ordered interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64
+}
+
+func Max[T Ordered](max T) func(T) error {
+	return func(v T) error {
+		if v > max {
+			return fmt.Errorf("value cannot be larger than %v", max)
+		}
+
+		return nil
+	}
+}
+
+func Min[T Ordered](min T) func(T) error {
+	return func(v T) error {
+		if v < min {
+			return fmt.Errorf("value cannot be smaller than %v", min)
+		}
+
+		return nil
+	}
+}
